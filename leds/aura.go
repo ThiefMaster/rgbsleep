7542b97e36047fc931598f9df76c8bc7f192b037
Adrian Moennich <adrian@planetcoding.net>
2018-12-05 07:24:31 +0100

    temp

--- leds/aura.go
+++ leds/aura.go
@@ -1,20 +1,34 @@
 package leds
 
 /*
-#cgo windows LDFLAGS: AURA_SDK.dll
-
 #include "AURA_SDK.h"
+
+static EnumerateMbControllerFunc EnumerateMbController;
+static SetMbModeFunc SetMbMode;
+static SetMbColorFunc SetMbColor;
+static GetMbLedCountFunc GetMbLedCount;
 
 static MbLightControl handle;
 static DWORD ledCount;
 static BYTE *colors;
 static BYTE ready;
 
+
+static void initDLL() {
+	HMODULE dll = LoadLibrary("AURA_SDK.dll");
+	EnumerateMbControllerFunc EnumerateMbController = (EnumerateMbControllerFunc)GetProcAddress(dll, "EnumerateMbController");
+	SetMbModeFunc SetMbMode = (SetMbModeFunc)GetProcAddress(dll, "SetMbMode");
+	SetMbColorFunc SetMbColor = (SetMbColorFunc)GetProcAddress(dll, "SetMbColor");
+	GetMbLedCountFunc GetMbLedCount = (GetMbLedCountFunc)GetProcAddress(dll, "GetMbLedCount");
+}
+
 BYTE Init() {
+	initDLL();
 	DWORD count = EnumerateMbController(NULL, 0);
 	if (count != 1) {
 		return 1;
 	}
+	return 2;
 	MbLightControl *handles = calloc(count, sizeof(MbLightControl));
 	EnumerateMbController(handles, count);
 	handle = handles[0];
