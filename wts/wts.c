24dd057b586631380ed5218d2fbc49eaa9e314ca
Adrian Moennich <adrian@planetcoding.net>
2018-12-08 20:36:09 +0100

    Use tab indent in wts.c

--- wts/wts.c
+++ wts/wts.c
@@ -4,8 +4,8 @@
 
 // missing in wtsapi32.h
 typedef enum _WTS_VIRTUAL_CLASS {
-    WTSVirtualClientData,
-    WTSVirtualFileHandle
+	WTSVirtualClientData,
+	WTSVirtualFileHandle
 } WTS_VIRTUAL_CLASS;
 
 #include <windows.h>
@@ -15,46 +15,46 @@ typedef enum _WTS_VIRTUAL_CLASS {
 
 LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
-    int wmId, wmEvent;
-    if (message == WM_WTSSESSION_CHANGE) {
-        relayMessage(wParam);
-        return 0;
-    } else {
-        return DefWindowProc(hWnd, message, wParam, lParam);
-    }
+	int wmId, wmEvent;
+	if (message == WM_WTSSESSION_CHANGE) {
+		relayMessage(wParam);
+		return 0;
+	} else {
+		return DefWindowProc(hWnd, message, wParam, lParam);
+	}
 }
 
 
 DWORD WINAPI WatchSessionNotifications(LPVOID lpParam)
 {
-    char const *lpClassName = "WTSMonitor";
+	char const *lpClassName = "WTSMonitor";
 
-    WNDCLASS wc;
-    wc.lpfnWndProc = WndProc;
-    wc.lpszClassName = lpClassName;
-    if (!RegisterClass(&wc)) {
-        return 0;
-    }
+	WNDCLASS wc;
+	wc.lpfnWndProc = WndProc;
+	wc.lpszClassName = lpClassName;
+	if (!RegisterClass(&wc)) {
+		return 0;
+	}
 
-    HWND hwnd = CreateWindow(
-        lpClassName, "WTS Monitor", 0, CW_USEDEFAULT, CW_USEDEFAULT, 0, 0, HWND_MESSAGE, NULL, NULL, NULL);
-    if (!hwnd) {
-        return 0;
-    }
+	HWND hwnd = CreateWindow(
+		lpClassName, "WTS Monitor", 0, CW_USEDEFAULT, CW_USEDEFAULT, 0, 0, HWND_MESSAGE, NULL, NULL, NULL);
+	if (!hwnd) {
+		return 0;
+	}
 
-    UpdateWindow(hwnd);
-    WTSRegisterSessionNotification(hwnd, NOTIFY_FOR_THIS_SESSION);
+	UpdateWindow(hwnd);
+	WTSRegisterSessionNotification(hwnd, NOTIFY_FOR_THIS_SESSION);
 
-    MSG msg;
-    while (GetMessage(&msg, NULL, 0, 0) > 0) {
-        TranslateMessage(&msg);
-        DispatchMessage(&msg);
-    }
+	MSG msg;
+	while (GetMessage(&msg, NULL, 0, 0) > 0) {
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
 }
 
 
 void Start() {
-    DWORD lpThreadId, lpParameter = 1;
-    CreateThread(NULL, 0, WatchSessionNotifications, &lpParameter, 0, &lpThreadId);
+	DWORD lpThreadId, lpParameter = 1;
+	CreateThread(NULL, 0, WatchSessionNotifications, &lpParameter, 0, &lpThreadId);
 
 }
